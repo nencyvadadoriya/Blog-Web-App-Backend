abede461a15d2b2dce36a915a6bd28eebc77340f
Nency Vadadoriya <vadadoriyanency8@gmail.com>
2025-11-12 22:29:34 +0530

    blog_backend_app

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require('express');
 const morgan = require("morgan")
 require("./config/db.confing")
+const cors = require("cors")
 const app = express();
 const PORT = process.env.PORT || 9000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 
 app.use('/api', require('./routes/index'))
 app.listen(PORT,(error)=>{ 
